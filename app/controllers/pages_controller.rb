68f15ffa903e9c480fcfe03ca1cce87f2476a284
rdtbui <richard.bui@member.mensa.org>
2022-01-16 16:15:41 -0500

    Generate report

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,19 @@
 class PagesController < ApplicationController
+    before_action :generate_report, only: [:report]
     def home
     end
     
     def report
+    end 
+
+    private
+    def generate_report
+        @items = Item.where("created_at >= :start_date AND created_at <= :end_date", { start_date: params[:start_date], end_date: params[:end_date] })
+        @most_in_stock = most_in_stock(@items)
+
+    end
+
+    def most_in_stock(items)
+        @items.maximum("quantity")
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root 'pages#home'
   get 'report', to: 'pages#report'
+  post 'report', to: 'pages#report'
   
   resources :items
 end
