d51e2b1e6c2394702baed31002c24f875d8f0dd1
rdtbui <richard.bui@member.mensa.org>
2022-01-16 15:12:15 -0500

    Whitelist Brand in item controller

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -65,6 +65,6 @@ class ItemsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def item_params
-      params.require(:item).permit(:title, :summary, :sku, :price, :quantity, :category)
+      params.require(:item).permit(:title, :summary, :sku, :price, :quantity, :category, :brand)
     end
 end
