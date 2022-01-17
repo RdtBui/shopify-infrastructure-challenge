6c624ff3bd22a690d5c50d303985a64357a93f21
rdtbui <richard.bui@member.mensa.org>
2022-01-17 15:10:05 -0500

    Add create Item integration test

--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -1,16 +1,36 @@
 require "test_helper"
-require "date"
 
 class ReportTest < ActiveSupport::TestCase
-  # @log = Log.new(item_title: "Some title",
-  #               item_sku: "Some sku",
-  #               item_price: 1,
-  #               item_quantity: 1,
-  #               item_brand: "Some brand",
-  #               item_category: "Some category",
-  #               created_at: Date.new(2021, 1, 1),
-  #               updated_at: )
-  # @log
+  def setup 
+    @log = Log.new(item_title: "Some title",
+                   item_sku: "Some sku",
+                   item_price: 1,
+                   item_quantity: 1,
+                   item_brand: "Some brand",
+                   item_category: "Some category")
+      
+    @log2 = Log.new(item_title: "Some title 2",
+                   item_sku: "Some sku 2",
+                   item_price: 1,
+                   item_quantity: 1,
+                   item_brand: "Some brand 2",
+                   item_category: "Some category 2")
+
+    @log3 = Log.new(item_title: "Some title 3",
+                    item_sku: "Some sku 3",
+                    item_price: 1,
+                    item_quantity: 1,
+                    item_brand: "Some brand 2",
+                    item_category: "Some category")
+
+    @log3 = Log.new(item_title: "Some title 4",
+                    item_sku: "Some sku 4",
+                    item_price: 1,
+                    item_quantity: 1,
+                    item_brand: "Some brand 3",
+                    item_category: "Some category 2")
+  end
+
 
   # generate_inventory_report tests
   # test "title should be present" do
@@ -31,5 +51,10 @@ class ReportTest < ActiveSupport::TestCase
   # min_max_quantity tests
   # min_max_revenue tests
   # min_max_average_quantity_by_criteria tests
+  
+  # test "boop" do
+  #   @item = {}
+  #   assert_not @item.valid?
+  # end
 
 end
