e8d53f2afb635da69f36ad55c2d7383051b29565
rdtbui <richard.bui@member.mensa.org>
2022-01-17 13:58:25 -0500

    Refactor report model

--- app/models/report.rb
+++ app/models/report.rb
@@ -27,78 +27,52 @@ class Report < ApplicationRecord
         return report
     end
 
-    # Return item logs with highest quantities
     def max_quantity(year, month)
         log = get_month_log(year, month)
-
-        if log.nil?
-            return nil
-        end
-        
-        return log.order(item_quantity: :desc).first
+        max_item = min_max_quantity(log, true)
+        return max_item
     end
     
     def max_revenue(year, month)
         log = get_month_log(year, month)
-        total_values = log.group("id").sum("item_quantity * item_price") # Total quantity per brand
-        log_id, revenue = total_values.max_by(&:first)
-
-        if log_id.nil?
-            return nil
-        end
-        
-        return Log.find(log_id)
+        max_item = min_max_revenue(log, true)
+        return max_item
     end
 
     def max_average_quantity_by_brand(year, month)
         log = get_month_log(year, month)
         max = min_max_average_quantity_by_criteria(:item_brand, log, true)
-        
         return max
     end
-
+    
     
     def max_average_quantity_by_category(year, month)
         log = get_month_log(year, month)
         max = min_max_average_quantity_by_criteria(:item_category, log, true)
-        
         return max
     end
     
     def min_quantity(year, month)
         log = get_month_log(year, month)
-        
-        if log.nil?
-            return nil
-        end
-        
-        return log.order(item_quantity: :asc).first
+        min_item = min_max_quantity(log, false)
+        return min_item
     end
     
     def min_revenue(year, month)
         log = get_month_log(year, month)
-        
-        total_values = log.group("id").sum("item_quantity * item_price") # Total quantity per brand
-        log_id, revenue = total_values.min_by(&:last)
-        
-        if log_id.nil?
-            return nil
-        end
-        
-        return Log.find(log_id)
+        min_item = min_max_revenue(log, false)
+        return min_item
     end
     
     def min_average_quantity_by_brand(year, month)
         log = get_month_log(year, month)
         min = min_max_average_quantity_by_criteria(:item_brand, log, false)
-        
         return min
     end
     
     def min_average_quantity_by_category(year, month)    
         log = get_month_log(year, month)
         min = min_max_average_quantity_by_criteria(:item_category, log, false)
-        
         return min
     end
     
@@ -107,15 +81,42 @@ class Report < ApplicationRecord
         end_date = DateTime.civil(year, month, -1, -1, -1) # Last day of the month, at 11:59 pm
         log = Log.where("created_at >= :start_date AND created_at <= :end_date", 
             { start_date: start_date, end_date: end_date })
+
+        return log
+    end
+
+    def min_max_quantity(log, max=false) # true => returns max, false => returns min
+        if log.nil?
+            return nil
+        end
+
+        if max
+            return log.order(item_quantity: :desc).first
+        end
+
+        return log.order(item_quantity: :asc).first
+    end
+
+    def min_max_revenue(log, max=false) # true => returns max, false => returns min
+        total_values = log.group("id").sum("item_quantity * item_price")
+        if max
+            log_id, revenue = total_values.max_by(&:first)
+        else
+            log_id, revenue = total_values.min_by(&:last)
         end
         
+        if log_id.nil?
+            return nil
+        end
+        
+        return Log.find(log_id)
     end
     
     def min_max_average_quantity_by_criteria(criteria, log, max = false) # true => returns max, false => returns min
-        average_quantity_per_sku = log.group(criteria, :item_sku).average("item_quantity") # Total quantity per brand
+        average_quantity_per_sku = log.group(criteria, :item_sku).average("item_quantity")
         total_average_quantity_per_criteria = {}
         
-        # Sums the average quantity of each SKU, grouped by brand
+        # Sums the average quantity of each SKU, grouped by the criteria
         average_quantity_per_sku.each do |key, value|
             if total_average_quantity_per_criteria[key[0]].nil?
                 total_average_quantity_per_criteria[key[0]] = value
@@ -123,12 +124,13 @@ class Report < ApplicationRecord
                 total_average_quantity_per_criteria[key[0]] += value
             end
         end
-
+        
         total_average_quantity_per_criteria.delete(nil)
-
+        
         if max
             return total_average_quantity_per_criteria.max_by(&:first)
         end
-
+        
         return total_average_quantity_per_criteria.min_by(&:last)
     end
+end
