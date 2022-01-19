4c44e56d4c3b72069bd9c3e3c320c9ef2489906f
rdtbui <richard.bui@member.mensa.org>
2022-01-18 22:35:51 -0500

    Update selecting year

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -2,7 +2,13 @@ require 'date'
 
 class ReportsController < ApplicationController
     def index
-        @report = Report.new.generate_year_report(params[:year].to_i)
+        if params[:year].nil?
+            @year = 2021
+        else
+            @year = params[:year].to_i
+        end
+
+        @report = Report.new.generate_year_report(@year)
     end 
     
 end
